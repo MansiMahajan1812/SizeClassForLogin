c05ed74f5edaf4588cc3eebf20a69faee2e96e11
Mansi Mahajan <Mansi@amrits-mac-mini.local>
2018-06-26 18:03:47 +0530

    Initial Commit

---
+++ SizeClassForLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassForLogin
+//
+//  Created by Mansi Mahajan on 6/26/18.
+//  Copyright © 2018 Mansi Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
